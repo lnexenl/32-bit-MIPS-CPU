4fe85cbc3de5c4dd505e8eb61e84aabeaa6e70b4
Sweetyshadow <Shadow_8890@126.com>
2018-07-06 17:23:14 +0800

    PCSrc in Control.v partly finished(exception and interrupt  excepted)

--- Single/Control.v
+++ Single/Control.v
@@ -2,7 +2,7 @@
 module Control(OpCode, Funct,
 	PCSrc, Branch, RegWrite, RegDst, 
 	MemRead, MemWrite, MemtoReg, 
-	ALUSrc1, ALUSrc2, ExtOp, LuOp, ALUOp);
+	ALUSrc1, ALUSrc2, ExtOp, LuOp);
 	input [5:0] OpCode;
 	input [5:0] Funct;
 	output [1:0] PCSrc;
@@ -16,12 +16,10 @@ module Control(OpCode, Funct,
 	output ALUSrc2;
 	output ExtOp;
 	output LuOp;
-	output [3:0] ALUOp;
 	
-	// Your code below
-	assign PCSrc = ((OpCode == 0)&&(Funct == 6'h08 || Funct == 6'h09))?2'b10:
-				  (OpCode == 6'h02 || OpCode == 6'h03)?2'b01:
-				  2'b00;
+	assign PCSrc = (OpCode == 6'h01 || (OpCode >= 6'h04 && OpCode <= 6'h07))?3'd1:
+				   (OpCode >= 6'h02 && OpCode <= 6'h03)?3'd2:
+				   (Funct >= 6'h08 && Funct <= 6'h09)?3'd3:3'd0
 	
 	assign Branch = (OpCode == 6'h04)?1:0;
 
@@ -44,16 +42,6 @@ module Control(OpCode, Funct,
 	assign ExtOp = (OpCode == 6'h23||OpCode == 6'h2b||OpCode == 6'h08||OpCode == 6'h09||OpCode == 6'h0c||OpCode == 6'h0a||OpCode == 6'h04)?1:0;
 
 	assign LuOp = (OpCode == 6'h0f)?1:0;
-	// Your code above
-	
-	assign ALUOp[2:0] = 
-		(OpCode == 6'h00)? 3'b010: 
-		(OpCode == 6'h04)? 3'b001: 
-		(OpCode == 6'h0c)? 3'b100: 
-		(OpCode == 6'h0a || OpCode == 6'h0b)? 3'b101: 
-		3'b000;
-		
-	assign ALUOp[3] = OpCode[0];
 	
 	assign ALUFun = (Funct == 6'h22 || Funct == 6'h23)?6'b000001:
 					(Funct == 6'h24 || OpCode == 6'h0c)?6'b011000:
