4146e0b2c94a42993f9155d0608006193fe7c6bd
Sweetyshadow <Shadow_8890@126.com>
2018-07-06 22:44:38 +0800

    interrupt and exception added

--- Single/Control.v
+++ Single/Control.v
@@ -1,12 +1,10 @@
-
 module Control(OpCode, Funct,
-	PCSrc, Branch, RegWrite, RegDst, 
+	PCSrc, RegWrite, RegDst, 
 	MemRead, MemWrite, MemtoReg, 
-	ALUSrc1, ALUSrc2, ExtOp, LuOp);
+	ALUSrc1, ALUSrc2, ExtOp, LuOp, Interrupt, ker, IRQ);
 	input [5:0] OpCode;
 	input [5:0] Funct;
 	output [1:0] PCSrc;
-	output Branch;
 	output RegWrite;
 	output [1:0] RegDst;
 	output MemRead;
@@ -16,26 +14,31 @@ module Control(OpCode, Funct,
 	output ALUSrc2;
 	output ExtOp;
 	output LuOp;
-	
+	output Interrupt;
+	wire Exception;
+	assign Exception = ~((OpCode == 6'h0 &&(Funct == 6'h0 || (Funct >= 6'h20 && Funct <= 6'h27) || Funct == 6'h02 || Funct == 6'h03 || Funct == 6'h2a || Funct == 6'h08 || Funct == 6'h09)) || (OpCode >= 6'h01 && OpCode <= 6'h0c) || OpCode == 6'h0f || OpCode == 6'h23 || OpCode == 6'h2b)
+	assign Interrupt = IRQ&&(~ker)
 	assign PCSrc = (OpCode == 6'h01 || (OpCode >= 6'h04 && OpCode <= 6'h07))?3'd1:
 				   (OpCode >= 6'h02 && OpCode <= 6'h03)?3'd2:
-				   (Funct >= 6'h08 && Funct <= 6'h09)?3'd3:3'd0
+				   (Funct >= 6'h08 && Funct <= 6'h09)?3'd3:
+				   Interrupt:3'd4:3'd0
 	
-	assign RegWrite = (OpCode == 6'h2b || (OpCode >= 6'h04 && OpCode <= 6'h07) || OpCode == 6'h02 || OpCode == 6'h01 || (OpCode == 0 && Funct == 6'h08))?0:1;
+	assign RegWrite = Interrupt || (OpCode == 6'h2b || (OpCode >= 6'h04 && OpCode <= 6'h07) || OpCode == 6'h02 || OpCode == 6'h01 || (OpCode == 0 && Funct == 6'h08))?0:1;
 
-	assign RegDst = (OpCode == 6'h03)?2'd2:
+	assign RegDst = (Interrupt || Exception)?2'd3:
+					(OpCode == 6'h03)?2'd2:
 					(OpCode == 6'h00)?2'd0:2'd1;
 	
-	assign MemRead = (OpCode == 6'h23)? 1: 0;
+	assign MemRead = (~Interrupt) || (OpCode == 6'h23);
 
-	assign MemWrite = (OpCode == 6'h2b)? 1: 0;
+	assign MemWrite = (~Interrupt) || (OpCode == 6'h2b);
 
-	assign MemtoReg = (OpCode == 6'h03 || (OpCode == 6'h00 && Funct == 6'h09))?2'd2:
+	assign MemtoReg = (OpCode == 6'h03 || (OpCode == 6'h00 && Funct == 6'h09) || (Interrupt || Exception))?2'd2:
 					  (OpCode == 6'h23)?2'd1:2'd0;
 
-	assign ALUSrc1 = (OpCode == 6'h00 && (Funct == 6'h00||Funct == 6'h02||Funct == 6'h03))
+	assign ALUSrc1 = (OpCode == 6'h00 && (Funct == 6'h00||Funct == 6'h02||Funct == 6'h03));
 
-	assign ALUSrc2 = ~(OpCode >= 6'h00 && OpCode <= 6'h07)
+	assign ALUSrc2 = ~(OpCode >= 6'h00 && OpCode <= 6'h07);
 
 	assign ExtOp = (OpCode == 6'h23||OpCode == 6'h2b||OpCode == 6'h08||OpCode == 6'h0a||(OpCode >= 6'h04 && OpCode <= 6'h07)||OpCode == 6'h01)?1:0;
 
@@ -54,5 +57,5 @@ module Control(OpCode, Funct,
 					(OpCode == 6'h0a || OpCode == 6'h0b || Funct == 6'h2a)?6'b110101:
 					(OpCode == 6'h06)?6'b111101:
 					(OpCode == 6'h07)?6'b111011:
-					(OpCode == 6'h01)?6'b111111:6'b000000
+					(OpCode == 6'h01)?6'b111111:6'b000000;
 endmodule
