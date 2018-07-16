43f468f6f84cb7511c29bd7b37755b44303445d0
wanghaofrank <1261674433@qq.com>
2018-07-16 19:17:56 +0800

    add lui

--- Single/Control.v
+++ Single/Control.v
@@ -1,7 +1,7 @@
 module Control(OpCode, Funct, ker, IRQ,
 	PCSrc, RegWrite, RegDst, 
 	MemRead, MemWrite, MemtoReg, 
-	ALUSrc1, ALUSrc2, ExtOp, LuOp, ALUFun);
+	ALUSrc1, ALUSrc2, ExtOp, LuOp, ALUFun, sign);
 	input [5:0] OpCode;
 	input [5:0] Funct;
 	input ker;
@@ -17,6 +17,7 @@ module Control(OpCode, Funct, ker, IRQ,
 	output ExtOp;
 	output LuOp;
 	output [5:0] ALUFun;
+	output sign;
 	wire Exception;
 	wire Interrupt;
 	assign Exception = ~((OpCode == 6'h0 &&(Funct == 6'h0 || (Funct >= 6'h20 && Funct <= 6'h27) || Funct == 6'h02 || Funct == 6'h03 || Funct == 6'h2a || Funct == 6'h08 || Funct == 6'h09)) || (OpCode >= 6'h01 && OpCode <= 6'h0c) || OpCode == 6'h0f || OpCode == 6'h23 || OpCode == 6'h2b)
@@ -26,7 +27,7 @@ module Control(OpCode, Funct, ker, IRQ,
 				   (Funct >= 6'h08 && Funct <= 6'h09)?3'd3:
 				   Interrupt:3'd4:3'd0
 	
-	assign RegWrite = Interrupt || (OpCode == 6'h2b || (OpCode >= 6'h04 && OpCode <= 6'h07) || OpCode == 6'h02 || OpCode == 6'h01 || (OpCode == 0 && Funct == 6'h08))?0:1;
+	assign RegWrite = (Interrupt || (OpCode == 6'h2b || (OpCode >= 6'h04 && OpCode <= 6'h07) || OpCode == 6'h02 || OpCode == 6'h01 || (OpCode == 0 && Funct == 6'h08))?0:1;
 
 	assign RegDst = (Interrupt || Exception)?2'd3:
 					(OpCode == 6'h03)?2'd2:
@@ -52,6 +53,7 @@ module Control(OpCode, Funct, ker, IRQ,
 					(Funct == 6'h25)?6'b011110:
 					(Funct == 6'h26)?6'b010110:
 					(Funct == 6'h27)?6'b010001:
+					(OpCode == 6'h0f)?6'b011010:
 					(Funct == 6'h00 && OpCode == 6'h00)?6'b100000:
 					(Funct == 6'h02)?6'b100001:
 					(Funct == 6'h03)?6'b100011:
@@ -61,4 +63,6 @@ module Control(OpCode, Funct, ker, IRQ,
 					(OpCode == 6'h06)?6'b111101:
 					(OpCode == 6'h07)?6'b111011:
 					(OpCode == 6'h01)?6'b111111:6'b000000;
+	
+	assign sign = (OpCode)?0:1; //to be continued
 endmodule
