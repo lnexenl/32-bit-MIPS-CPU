174ae684cb88081ddd1c67bf08bf32a838858ad0
lizr16 <37014812+lizr16@users.noreply.github.com>
2018-07-17 21:02:28 +0800

    use what it is in single

--- Pipeline/ALU.v
+++ Pipeline/ALU.v
@@ -1,141 +1,77 @@
-module ALU(A,B,Z,Sign,ALUFun);
+module ALU(in1, in2, out, sign, funct);
+input in1, in2, sign, funct;
+output out;
+wire [31:0]in1;
+wire [31:0]in2;
+wire [31:0]in2_2;
+wire [5:0]funct;
+wire sign;
+reg [31:0]out;
+wire [31:0]out_1;
+wire [32:0]out_2;
+wire [32:0]in1_ext;
+wire [32:0]in2_ext;
+wire [32:0]in2_ext_2;
+reg [63:0] out_ext;
+wire [32:0] out_ext_;
+wire Z, V, N;
 
-input [31:0] A;
-
-input [31:0] B;
-
-input [5:0] ALUFun;
-
-input Sign;
-
-output [31:0] Z;
-
-wire Y=1'b0,Y1=1'b0,Y2=1'b0;
-
-wire N=1'b0,N1=1'b0,N2=1'b0;
-
-wire [31:0] add;
-
-wire [31:0] sub;
-
-wire [32:0] IN1=9'h000000000;
-
-wire [32:0] IN2=9'h000000000;
-
-wire [32:0] OUT1=9'h000000000;
-
-wire [32:0] OUT2=9'h000000000;
-
-//如果溢出就直接溢出位就可以了
-
-assign add = A + B;
-
-assign IN1[31:0] = A;
-
-assign IN2[31:0] = B;
-
-assign OUT1 = IN1 + IN2;
-
-assign OUT2 = IN1 - IN2;
-
-assign N1 = (Sign==1'b1&&add[31]==1'b1)?1'b1:1'b0;
-
-assign N2 = (Sign==1'b1&&sub[31]==1'b1)?1'b1:1'b0;
-
-assign V1 = (OUT1[32]==1'b1)?1'b1:1'b0;
-
-assign V2 = (OUT2[32]==1'b1)?1'b1:1'b0;
-
-
-
+assign in1_ext[31:0] = in1;
+assign in2_ext[31:0] = in2;
+assign in1_ext[32] = 0;
+assign in2_ext[32] = 0;
+assign in2_ext_2 = ~in2_ext + 1;//取二补码对无符号数进行计算
+assign out_ext_ = in1_ext + in2_ext_2; //out_ext= A - B
+assign in2_2 = ~in2 + 1;//取二补码对有符号数进行计算
+assign out_1 = in1 + in2;// A + B
+assign out_2 = in1 + in2_2; // A - B
+assign Z = (in1 == in2);
+assign N = sign & out_2[31];
+assign V = (sign&&(in1[31]&&in2_2[31]&&(out_2[31]==0)||(in1[31]==0)&&(in2_2[31]==0)&&out_2[31]))||((~sign)&&out_ext_[32]);
 always @(*)
-
-case (ALUFun)
-
-	6'b000000: //ADD
-
-	begin
-
-		Z <= add;
-
-		Y <= Y1;
-
-		if(Sign==1'b1) N <= N1;
-
-	end
-
-	else N <= 1'b0;
-
-	6'b000001: //SUB
-
-	begin 
-
-		Z <= sub;
-
-		Y <= Y2;
-
-		if (Sign == 1'b1) N <= N2;
-
-		else N <= 1'b0;
-
-	end
-
-	6'b011000: Z <= A & B;
-
-	6'b011110: Z <= A | B;
-
-	6'b010110: Z <= A ^ B;
-
-	6'b010001: Z <=~(A|B);
-
-	6'b011010: Z <= A    ;
-
-	6'b100000: Z <= (B << A[4:0]);
-
-	6'b100001: Z <= (B >> A[4:0]);
-
-	6'b100011: Z <= ({{32{B[31]}}, B} >> A[4:0]);
-
-	6'b110011:
-
-	begin
-
-		Z = sub; 
-
-		Z <= (Z==8'h00000000)?8'h000000001:8'h00000000;
-
-	end
-
-	6'b110001:
-
-	begin 
-
-		Z = sub;
-
-		Z <= (Z!=8'h00000000)?8'h000000001:8'h00000000;
-
-	end
-
-	6'b110101:
-
-	begin
-
-		Z = sub;
-
-		Z <= (Z[0])?8'h000000001:8'h00000000;
-
-	end
-
-	6'b111101: Z <= ((Sign==1'b1&&A[31]==1'b1)||A==8'h00000000)?8'h000000001:8'h00000000;
-
-	6'b111011: Z <= (Sign==1'b1&&A[31]==1'b1)?8'h000000001:8'h00000000;
-
-	6'b111111: Z <= (((Sign==1'b1&&A[31]==1'b0)||(Sign==1'b0))&&(A!=8'h00000000))?8'h000000001:8'h00000000;
-
-	default: Z <= 32'h00000000;
-
+begin
+case (funct)
+    6'b000000: out <= in1 + in2;
+    6'b000001: out <= in1 + in2_2;
+    6'b011000: out <= in1 & in2;
+    6'b011110: out <= in1 | in2;
+    6'b010110: out <= in1 ^ in2;
+    6'b010001: out <= ~(in1|in2);
+    6'b011010: out <= in1;
+    6'b100000: begin
+            out_ext = {32'b0, in2};
+            if (in1[0]) out_ext = out_ext << 1;
+            if (in1[1]) out_ext = out_ext << 2;
+            if (in1[2]) out_ext = out_ext << 4;
+            if (in1[3]) out_ext = out_ext << 8;
+            if (in1[4]) out_ext = out_ext << 16;
+            out <= out_ext[31:0];
+    end
+    6'b100001: begin
+            out_ext = {32'b0, in2};
+            if (in1[0]) out_ext = out_ext >> 1;
+            if (in1[1]) out_ext = out_ext >> 2;
+            if (in1[2]) out_ext = out_ext >> 4;
+            if (in1[3]) out_ext = out_ext >> 8;
+            if (in1[4]) out_ext = out_ext >> 16;
+            out <= out_ext[31:0];
+    end
+    6'b100011: begin
+            out_ext = {{32{in2[31]}}, in2};
+            if (in1[0]) out_ext = out_ext >> 1;
+            if (in1[1]) out_ext = out_ext >> 2;
+            if (in1[2]) out_ext = out_ext >> 4;
+            if (in1[3]) out_ext = out_ext >> 8;
+            if (in1[4]) out_ext = out_ext >> 16;
+            out <= out_ext[31:0];
+    end
+    6'b110011: out <= (Z==1)?1:0;
+    6'b110001: out <= (Z==1)?0:1;
+    6'b110101: out <= (N==1)?1:0;
+    6'b111101: out <= (N==1||Z==0)?1:0;
+    6'b111101: out <= ((in1[31] == 1||in1 == 32'b0)&&sign == 1)?1:0;
+    6'b111011: out <= (in1[31] == 1 && sign == 1)?1:0;
+    6'b111111: out <= ((in1[31] == 1||in1 == 32'b0)&&sign == 1)?0:1;
 endcase
-
-
-
+end
 endmodule
